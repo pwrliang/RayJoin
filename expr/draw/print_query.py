719f4539f6f50a2a7c6ef5d3a30ddd71faf2dc6c
Liang Geng <geng.161@osu.edu>
2024-01-18 11:34:11 -0500

    Print speedup

--- expr/draw/print_query.py
+++ expr/draw/print_query.py
@@ -11,13 +11,16 @@ l = ["dtl_cnty_USAZIPCodeArea_",
      "lakes_parks_North_America_",
      "lakes_parks_South_America_"]
 
+time_list = []
+total_time_list = []
+
 for m in ("grid", "lbvh", "rt"):
     load_time = []
     build_time = []
     ag_time = []
     query_time = []
     for n in l:
-        file = "query_pip/" + n + m + ".log"
+        file = "query_lsi/" + n + m + ".log"
         if os.path.exists(file):
             with open(file, 'r') as fi:
                 for line in fi:
@@ -44,10 +47,18 @@ for m in ("grid", "lbvh", "rt"):
     build_time = np.asarray(build_time)
     ag_time = np.asarray(ag_time)
     query_time = np.asarray(query_time)
-    print("Mode", m)
-    np.set_printoptions(formatter={'float': lambda x: "{0:0.0f}".format(x)})
     if m != "rt":
         ag_time = np.zeros(len(query_time))
+
+    print("Mode", m)
+    time_list.append(query_time)
+    total_time_list.append(load_time + build_time + ag_time + query_time)
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.0f}".format(x)})
+
     print("Preprocessing", load_time + ag_time + build_time)
     print("Processing", query_time)
     print("Total", load_time + ag_time + build_time + query_time)
+
+np.set_printoptions(formatter={'float': lambda x: "{0:0.1f}".format(x)})
+print("Process speedup", np.minimum(time_list[0], time_list[1]) / time_list[2])
+print("Preprocess speedup", np.minimum(total_time_list[0], total_time_list[1]) / total_time_list[2])
